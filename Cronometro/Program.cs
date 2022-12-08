d643b13a4b75193affeaff64bc3aad10cb503692
Diogo Bozan <diogo_fb_@hotmail.com>
2022-12-08 14:42:46 +0000

    cronometro finish

---
+++ Cronometro/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 - Exit");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char tipo = char.Parse(data.Substring(data.Length -1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(tipo == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if(time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Apontar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("GO!!");
+            Thread.Sleep(1000);
+
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        { 
+            int currentTime = 0;
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Cronometro Finalizado!");
+            Thread.Sleep(2000);
+            //System.Environment.Exit(0);
+            Menu();
+
+        }
+
+
+    }
+}
